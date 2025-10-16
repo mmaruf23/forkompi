3cc4c2837a8584a4f1b7398d4527af377fbc0b78
mmaruf <maruuf82@gmail.com>
2025-10-16 12:30:47 +0700

    test gak pake next/font

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Navbar from "../ui/navbar";
 import Footer from "../ui/footer";
-import { Hammersmith_One } from "next/font/google";
+// import { Hammersmith_One } from "next/font/google";
 import WhatsAppButton from "../ui/wa_button";
 type LayoutProps = {
   children: React.ReactNode;
 };
 
-const hammersmith = Hammersmith_One({
-  subsets: ["latin"],
-  weight: "400",
-});
+// const hammersmith = Hammersmith_One({
+//   subsets: ["latin"],
+//   weight: "400",
+// });
 // todo : nanti untuk page /admin mah jangan pake layout ini
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={`min-h-svh ` + hammersmith.className}>
+    <div className={`min-h-svh `}>
       <Navbar />
       <main className="flex items-center">{children}</main>
       <WhatsAppButton />
